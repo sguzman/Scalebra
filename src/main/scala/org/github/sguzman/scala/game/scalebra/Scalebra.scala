01ff90cf2e7a246c172602420aeac301d56f675d
Salvador Guzman <salvador.guzman@logicmonitor.com>
2016-04-20 12:47:05 -0700

    added stub driver clas

---
+++ src/main/scala/org/github/sguzman/scala/game/scalebra/Scalebra.scala
@@ -0,0 +1,14 @@
+package org.github.sguzman.scala.game.scalebra
+
+/**
+  * @author Salvador Guzman <guzmansalv@gmail.com>
+  *
+  * SCALEBRA - A Snake Clone
+  *
+  * Scalebra is a portmanteau of Scala and Culebra which is Spanish for snake.
+  * This is a simple, basic clone of snake.
+  *
+  */
+object Scalebra {
+  def main(args: Array[String]) = println("Hello, world")
+}
