404ca29e8d0db276315309082471303c76ef5529
Salvador Guzman <salvador.guzman@logicmonitor.com>
2016-05-08 00:57:52 -0700

    make class private override vars

--- src/main/scala/org/github/sguzman/scala/game/scalebra/mvc/model/snake/SnakePiece.scala
+++ src/main/scala/org/github/sguzman/scala/game/scalebra/mvc/model/snake/SnakePiece.scala
@@ -9,5 +9,5 @@ import org.github.sguzman.scala.game.scalebra.mvc.model.artifact.Entity
   * @note This class holds an individual snake piece. This is a simple square
   * @since 5/7/16 11:46 PM
   */
-class SnakePiece(r: Float, g: Float, b: Float, var x: Float, var y: Float)
+private class SnakePiece(r: Float, g: Float, b: Float, override var x: Float, override var y: Float)
   extends Entity(r, g, b, x, y)
