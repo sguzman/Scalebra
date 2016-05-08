3e57d04c9a692ea735d124dbbe703d447cc479a4
Salvador Guzman <salvador.guzman@logicmonitor.com>
2016-05-08 16:42:29 -0700

    renamed Pause to PauseDir

--- src/main/scala/org/github/sguzman/scala/game/scalebra/mvc/view/pause/PauseDir.scala
+++ src/main/scala/org/github/sguzman/scala/game/scalebra/mvc/view/pause/PauseDir.scala
@@ -7,8 +7,8 @@ package org.github.sguzman.scala.game.scalebra.mvc.view.pause
   * @note Pause directives for akka actors
   * @since 5/8/16 4:19 PM
   */
-abstract sealed class PauseMe
+abstract sealed class PauseDir
 
-case class Pause() extends PauseMe
-case class UnPause() extends PauseMe
-case class TogglePause() extends PauseMe
+case class Pause() extends PauseDir
+case class UnPause() extends PauseDir
+case class TogglePause() extends PauseDir
